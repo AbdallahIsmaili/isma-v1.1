10c1ed5b25f56c7c9337bbf3e114aa3f2946cd15
AbdallahIsmaili <89218851+AbdallahIsmaili@users.noreply.github.com>
2022-10-16 02:21:06 +0100

    start

---
+++ src/components/Hero.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Image from "../assets/img/hero-bg.png";
+// import {IoIosArrowDroprightCircle} from "react-icons/io";
+
+const Hero = () => {
+  return <section>
+    <div className="container mx-auto">
+      <div>
+        <div>text</div>
+        <div>image</div>
+      </div>
+    </div>
+  </section>;
+};
+
+export default Hero;
