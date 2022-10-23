0781c95f0fa2ac7a02b60ee92f39b437f9cfe786
AbdallahIsmaili <89218851+AbdallahIsmaili@users.noreply.github.com>
2022-10-23 23:22:58 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import Hero from './components/Hero';
-// import './App.css';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 
 const App = () => {
+
+  useEffect(() => {
+    Aos.init({
+      duration: 2500,
+      delay: 400,
+    })
+  })
   return (
     <div className="overflow-hidden">
       <Hero />
